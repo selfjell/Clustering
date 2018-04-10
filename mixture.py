924e7c5ef1b0733d0ac77356fa533e2bf434f481
Havard <h.trondsen@hotmail.com>
2018-04-10 16:25:08 +0200

    initial dataset importation

--- mixture.py
+++ mixture.py
@@ -1,4 +1,28 @@
-from sklearn import mixture
+from sklearn.mixture import GaussianMixture
 from pathlib import Path
 
-def get_features()
+def get_features(line,set_):
+    list_ = line.split("	")
+    if list_[7] == set_:
+        for i in range(7):
+            list_[i] = float(list_[i])
+    return list_[0:7]
+
+def get_dataset(set_):
+    path = Path("..").joinpath("seeds_dataset.txt")
+    lines = path.open("r").readlines()
+    data = []
+    for line in lines:
+        example = get_features(line,set_)
+        data.append(example)
+    return data
+
+train = get_dataset("1")
+test = get_dataset("3")
+gm = GaussianMixture(n_components = 7)
+
+gm.fit(X = train)
+
+ar = gm.predict(X=test)
+print(ar)
+print("Size: {}".format(len(ar)))
